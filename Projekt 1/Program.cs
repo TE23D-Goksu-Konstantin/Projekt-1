28e19644e507bf4040a8f02ac5b007f66f5f3bab
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-01-20 09:26:51 +0100

    Added the writing of letters singular in words (unrefined)

---
+++ Projekt 1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+void Main(string[] args)
+{
+
+    string vtext = "";
+        
+    for(int i = 0; i < vtext.Length; i++)
+    {
+        Console.Write(vtext[i]);
+        Thread.Sleep(75);
+    }
+   
+}
+
+
+
+
+

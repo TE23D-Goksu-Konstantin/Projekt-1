b650cbf23dd65e0344c8429919286fd2f644e249
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-01-20 09:51:39 +0100

    Fixed so that the writing method is always usable

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,16 +1,30 @@
 ﻿
 
+    
 
-void Main(string[] args)
+    string consoleOutput = "Hello adventurer, what should I call you by?";
+    writing(consoleOutput);
+
+    string name = Console.ReadLine();
+    consoleOutput = $"Greetings {name}!";
+    writing(consoleOutput);
+
+Console.ReadLine();
+
+
+
+
+static void writing(string text)
 {
 
-    string vtext = "";
         
-    for(int i = 0; i < vtext.Length; i++)
+    for(int i = 0; i < text.Length; i++)
     {
-        Console.Write(vtext[i]);
+        Console.Write(text[i]);
         Thread.Sleep(75);
     }
+
+    Console.WriteLine();
    
 }
 

88ec5b9db102295f7d87272b34607b8b1d860202
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-01-24 10:52:32 +0100

    slut på lektion commit

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,34 +1,82 @@
-﻿
-
-    
-
-    string consoleOutput = "Hello adventurer, what should I call you by?";
-    writing(consoleOutput);
-
-    string name = Console.ReadLine();
-    consoleOutput = $"Greetings {name}!";
-    writing(consoleOutput);
-
-Console.ReadLine();
+﻿using System;
 
 
 
+string consoleOutput = "Hello adventurer, what should I call you by?";
+writing(consoleOutput);
 
-static void writing(string text)
+
+string name = nameP();
+consoleOutput = $"Greetings {name}! Choose your starter weapon.";
+writing(consoleOutput);
+Console.Clear();
+
+List<string> weapons = new List<string> {};
+string w1 = bWeaponPick();
+weapons.Add($"{bWeaponPick}");
+
+Console.WriteLine(weapons[0]);
+
+
+
+static string nameP()
 {
-
-        
-    for(int i = 0; i < text.Length; i++)
+    while(true)
     {
-        Console.Write(text[i]);
-        Thread.Sleep(75);
+    string name = Console.ReadLine();
+        if(string.IsNullOrWhiteSpace(name) || int.TryParse(name, out int t))
+        {
+            Console.Clear();
+            string consoleOutput = "Error";
+            writing(consoleOutput);
+            continue;
+        }
+        else
+        {
+            return name;
+        }
     }
-
-    Console.WriteLine();
-   
 }
 
 
 
 
 
+
+static void writing(string text)
+{
+    for (int i = 0; i < text.Length; i++)
+    {
+        Console.Write(text[i]);
+        Thread.Sleep(60);
+    }
+    Console.WriteLine();
+}
+
+
+
+
+static string bWeaponPick()
+{
+
+    List<string> bWeapons = new List<string> { "Sword", "Axe", "Dagger", "Bow" };
+    for (int e = 0; e < bWeapons.Count; e++)
+    {
+        string consoleOutput = $"{e + 1}: " + $"{bWeapons[e]}";
+        writing(consoleOutput);
+    }
+
+    string pick = Console.ReadLine();
+    int.TryParse(pick, out int pickOut);
+    for (int e = 0; e < bWeapons.Count; e++)
+    {
+        if(pickOut == bWeapons[e])
+        {
+            
+        }
+    }
+
+}
+
+
+//slut på lektion commit

405aa6060f01836a29a9281648042d3ef6059e52
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-01-27 09:31:31 +0100

    weaponspick funkar

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -7,16 +7,21 @@ writing(consoleOutput);
 
 
 string name = nameP();
-consoleOutput = $"Greetings {name}! Choose your starter weapon.";
+consoleOutput = $"Greetings {name}!";
 writing(consoleOutput);
+Thread.Sleep(500);
 Console.Clear();
 
 List<string> weapons = new List<string> {};
+List<int> weaponsDmg = new List<int> {30, 50, 40, 20};
+List<int> weaponsHit = new List<int> {70, 50, 60, 100};
 string w1 = bWeaponPick();
-weapons.Add($"{bWeaponPick}");
+weapons.Add(w1);
 
-Console.WriteLine(weapons[0]);
+consoleOutput = $"{weapons[0]}";
+writing(consoleOutput);
 
+Console.ReadLine();
 
 
 static string nameP()
@@ -26,7 +31,6 @@ static string nameP()
     string name = Console.ReadLine();
         if(string.IsNullOrWhiteSpace(name) || int.TryParse(name, out int t))
         {
-            Console.Clear();
             string consoleOutput = "Error";
             writing(consoleOutput);
             continue;
@@ -58,24 +62,62 @@ static void writing(string text)
 
 static string bWeaponPick()
 {
+    while(true)
+    {
+    Console.Clear();
+    string consoleOutput = "Please choose your starter weapon.";
+    writing(consoleOutput);
 
+    List<int> weaponsDmg = new List<int> {30, 50, 40, 20};
+    List<int> weaponsHit = new List<int> {70, 50, 60, 100};
     List<string> bWeapons = new List<string> { "Sword", "Axe", "Dagger", "Bow" };
     for (int e = 0; e < bWeapons.Count; e++)
     {
-        string consoleOutput = $"{e + 1}: " + $"{bWeapons[e]}";
+        consoleOutput = $"{e + 1}: " + $"{bWeapons[e]}";
         writing(consoleOutput);
     }
 
-    string pick = Console.ReadLine();
-    int.TryParse(pick, out int pickOut);
-    for (int e = 0; e < bWeapons.Count; e++)
-    {
-        if(pickOut == bWeapons[e])
-        {
-            
+        string pick = Console.ReadLine();
+        
+
+            if(int.TryParse(pick, out int pickOut) && pickOut <= 5 && pickOut >=1)
+            {
+                Console.Clear();
+
+                if(pickOut == 1)
+                {
+                    consoleOutput = "Sword stats";
+                    writing(consoleOutput);
+                    consoleOutput = $"Damage: {weaponsDmg[0]}";
+                    writing(consoleOutput);
+                    consoleOutput = $"Hit chance: {weaponsHit[0]}";
+                    writing(consoleOutput);
+                    consoleOutput = "Are you sure? (y/n)";
+                    writing(consoleOutput);
+                    string bWeaponsPickCheck = Console.ReadLine();
+                    if(bWeaponsPickCheck.ToLower() == "y")
+                        {
+                        return bWeapons[pickOut - 1];
+                        }
+                    else if(bWeaponsPickCheck.ToLower() == "n")
+                        {
+                        continue;
+                        }
+                    else
+                    {
+                        consoleOutput = "Error";
+                        writing(consoleOutput);
+                        continue;
+                    }
+                }
+            }
+            else
+            {
+                consoleOutput = "Error";
+                writing(consoleOutput);
+                continue;
         }
     }
-
 }
 
 

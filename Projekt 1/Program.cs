9b09924dba5bcdc54ac2ae04736ebf2a6c9ae5a7
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2025-01-31 10:49:07 +0100

    spelarens olika stats är klar

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -2,6 +2,31 @@
 
 
 
+List<string> weapons = new List<string> {};
+List<int> weaponsDmg = new List<int> {};
+List<int> weaponsHit = new List<int> {};
+List<int> statBweaponsDmg = new List<int> {30, 50, 40, 20};
+List<int> statBweaponsHit = new List<int> {70, 50, 60, 100};
+
+
+Main();
+
+
+void Main()
+{
+    int hp = 100;
+    mainMeny();
+    Player player = new Player(hp, weaponsDmg[0], weaponsHit[0]); 
+    Console.WriteLine($"Your health: {player.playerhealth}, Your damage: {player.playerdamage}, Your hitchance: {player.playerhitChance}");
+    Console.ReadLine();
+    
+}
+
+
+void mainMeny()
+{
+
+
 string consoleOutput = "Hello adventurer, what should I call you by?";
 writing(consoleOutput);
 
@@ -12,26 +37,17 @@ writing(consoleOutput);
 Thread.Sleep(500);
 Console.Clear();
 
-List<string> weapons = new List<string> {};
-List<int> weaponsDmg = new List<int> {30, 50, 40, 20};
-List<int> weaponsHit = new List<int> {70, 50, 60, 100};
-string w1 = bWeaponPick();
-weapons.Add(w1);
+
+(string bWeapon, int bWeaponDmg, int bWeaponHit)= bWeaponPick();
+weapons.Add(bWeapon);
+weaponsHit.Add(bWeaponDmg);
+weaponsDmg.Add(bWeaponHit);
 
 consoleOutput = "You chose " + $"{weapons[0]}" + ", now it's time to begin your journey.";
 writing(consoleOutput);
 Thread.Sleep(1000);
 
-bool check = false;
-Proceed(check);
-
-
-Console.ReadLine();
-
-
-
-
-
+}
 
 
 static string nameP()
@@ -53,10 +69,6 @@ static string nameP()
 }
 
 
-
-
-
-
 static void writing(string text)
 {
     for (int i = 0; i < text.Length; i++)
@@ -68,9 +80,7 @@ static void writing(string text)
 }
 
 
-
-
-static string bWeaponPick()
+static (string, int, int) bWeaponPick()
 {
     while(true)
     {
@@ -106,8 +116,8 @@ static string bWeaponPick()
                     writing(consoleOutput);
                     string bWeaponsPickCheck = Console.ReadLine();
                     if(bWeaponsPickCheck.ToLower() == "y")
-                        {
-                        return bWeapons[pickOut - 1];
+                        {;
+                        return (bWeapons[e], weaponsDmg[e], weaponsHit[e]);
                         }
                     else if(bWeaponsPickCheck.ToLower() == "n")
                         {
@@ -129,7 +139,6 @@ static string bWeaponPick()
         }
     }
 }
-
 
 
 static bool Proceed(bool check)
@@ -163,4 +172,21 @@ static bool Proceed(bool check)
 
 
 
+
+
+
+class Player
+{
+    public int playerhealth{get; set;}
+    public int playerdamage{get; set;}
+    public double playerhitChance{get; set;}
+
+
+    public Player(int playerHealth, int playerDamage, double playerHitChance)
+    {
+        playerhealth = playerHealth;
+        playerdamage = playerDamage;
+        playerhitChance = playerHitChance;
+    }
+}
 //slut på lektion commit
